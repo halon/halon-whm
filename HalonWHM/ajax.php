e25e0bf10eb518b6e9317a14e912a7891afd3ec6
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2018-07-02 21:43:13 +0200

    Cleanup file

--- HalonWHM/ajax.php
+++ HalonWHM/ajax.php
@@ -1,24 +1,17 @@
 <?php 
 
 error_reporting(E_ALL);
-ini_set("display_errors", 0);
+ini_set('display_errors', 0);
+define('DS', DIRECTORY_SEPARATOR);
 
-define("DS",DIRECTORY_SEPARATOR);
+require_once '/usr/local/cpanel/php/cpanel.php';
+require_once '/usr/local/cpanel/share/Halon/HalonLoader.php';
 
-define("SmarterEmail_INCLUDES",'/usr/local/cpanel/share/Halon/');
-
-require_once "/usr/local/cpanel/php/cpanel.php";
-
-require_once SmarterEmail_INCLUDES.'HalonLoader.php';
-
-try
-{  
-    $Main = new HalonMainController('WHM',__DIR__);
-    $page   = empty($_REQUEST['page'])?null:$_REQUEST['page'];
-    $action = empty($_REQUEST['action'])?null:$_REQUEST['action'];
-    
-    echo $Main->getJSONResponse($page,$action,$_REQUEST);
-}
-catch(Exception $ex){
-    echo "Something Goes wrong";
+try {
+    $Main = new HalonMainController('WHM', __DIR__);
+    $page = empty($_REQUEST['page']) ? null : $_REQUEST['page'];
+    $action = empty($_REQUEST['action']) ? null : $_REQUEST['action'];
+    echo $Main->getJSONResponse($page, $action, $_REQUEST);
+} catch (Exception $ex) {
+    echo 'Something went wrong';
 }
